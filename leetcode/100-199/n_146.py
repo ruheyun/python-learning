054f5d37bf9ba5d67825ec061abde9cf2b8d7037
ruheyun <1654033661@qq.com>
2026-05-16 20:13:37 +0800

    20260516

---
+++ leetcode/100-199/n_146.py
@@ -0,0 +1,18 @@
+"""
+146. LRU 缓存
+"""
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        pass
+        
+
+    def get(self, key: int) -> int:
+        pass
+        
+
+    def put(self, key: int, value: int) -> None:
+        pass
+        
