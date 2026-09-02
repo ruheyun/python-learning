7a42a20da5f417d25e853412810206e1c2750c15
ruheyun <1654033661@qq.com>
2026-09-02 19:31:28 +0800

    20260902

---
+++ decorator.py
@@ -0,0 +1,18 @@
+class SingletonDecorator:
+    def __init__(self, cls):
+        self.cls = cls
+        self.instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self.instance is None:
+            self.instance = self.cls(*args, **kwargs)
+        return self.instance
+
+@SingletonDecorator
+class Database:
+    def __init__(self):
+        print("初始化")
+
+db1 = Database()
+db2 = Database()
+print(db1 is db2)
